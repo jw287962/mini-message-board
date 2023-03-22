58b46d0c03953833989ac39a07455971028b3b16
JasonPC <jasonwong287962@gmail.com>
2023-03-22 00:04:09 -0500

    removed message array

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+// const messages = [
+//   {
+//     text: "Hi there!",
+//     user: "Amando",
+//     added: new Date()
+//   },
+//   {
+//     text: "Hello World!",
+//     user: "Charles",
+//     added: new Date()
+//   }
+// ];
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', 
@@ -20,11 +20,5 @@ router.get('/', function(req, res, next) {
     messages: messages,  
   });
 });
-
-router.post('/new', function(req, res, next) {
- messages.push({text: req.body.message, user: req.body.name, added: new Date()});
- res.redirect('/');
-}
-)
 
 module.exports = router;
