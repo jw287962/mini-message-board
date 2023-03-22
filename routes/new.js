a4cace0c0f3eec68c1a94cc669649036a5bbdf3c
JasonPC <jasonwong287962@gmail.com>
2023-03-22 00:35:27 -0500

    change mongo to support callback version router post for form

--- routes/new.js
+++ routes/new.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const messageController = require('../controllers/messageController');
+
+router.post('/', messageController.message_create);
 
 router.get('/', function(req, res, next) {
   res.render('form', {
@@ -9,5 +12,13 @@ router.get('/', function(req, res, next) {
 });
 
 
+// router.get('/',)
+
+// router.post('/new', function(req, res, next) {
+//   messages.push({text: req.body.message, user: req.body.name, added: new Date()});
+//   res.redirect('/');
+//  }
+//  )
+ 
 
 module.exports = router;
