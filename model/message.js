f4ac7ca50c0179a913b14c823b0a93f274bcd5a5
JasonPC <jasonwong287962@gmail.com>
2023-03-22 00:46:05 -0500

    added date formatter

--- model/message.js
+++ model/message.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const {DateTime} = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -8,4 +9,8 @@ const MessageSchema = new Schema({
   added: {type: Date}
 })
 
+MessageSchema.virtual('added_format').get(function() {
+  return DateTime.fromJSDate(this.added).toLocaleString(DateTime.DATE_MED)
+
+});
 module.exports = mongoose.model('Message',MessageSchema)
