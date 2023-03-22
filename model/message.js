fc3b204cd7312eca12bf9194f5944ef50d9d8879
JasonPC <jasonwong287962@gmail.com>
2023-03-22 00:31:26 -0500

    schema updated

--- model/message.js
+++ model/message.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
   user: {type: String, required: true},
